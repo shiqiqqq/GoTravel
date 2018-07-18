0b22384410f7dd7bbefc54276b6bd12d0135da7d
geekerstar <247507792@qq.com>
2018-07-18 21:20:45 +0800

    城市选择页：通过Vuex的高级用法及localStorage优化代码

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -23,6 +23,7 @@
 
 <script>
   import Bscroll from 'better-scroll'
+  import { mapMutations} from 'vuex'
   export default {
     name: 'CitySearch',
     props:{
@@ -65,9 +66,10 @@
     },
     methods:{
       handleCityClick(city){
-        this.$store.commit('changeCity',city)
+        this.changeCity(city)
         this.$router.push('/')
-      }
+      },
+      ...mapMutations(['changeCity'])
     },
     mounted(){
       this.scroll = new Bscroll(this.$refs.search)
