7e578305868c6210dde91d34e92139cd66999023
geekerstar <247507792@qq.com>
2018-07-18 19:28:48 +0800

    城市选择页：页面的动态数据渲染

--- src/pages/city/components/list.vue
+++ src/pages/city/components/list.vue
@@ -12,32 +12,24 @@
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">北京</div>
+          <div
+            class="button-wrapper"
+            v-for="item of hot"
+            :key = "item.id"
+          >
+            <div class="button">{{item.name}}</div>
           </div>
-          <div class="button-wrapper">
-            <div class="button">上海</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">广州</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">深圳</div>
-          </div>
-
         </div>
       </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
+      <div class="area" v-for="(item,key) of cities" :key ="key">
+        <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
-          <div class="item border-bottom">
-          阿拉尔
-          </div>
-          <div class="item border-bottom">
-            阿拉尔
-          </div>
-          <div class="item border-bottom">
-            阿拉尔
+          <div
+            class="item border-bottom"
+            v-for="innerItem of item"
+            :key="innerItem.id"
+          >
+          {{innerItem.name}}
           </div>
         </div>
       </div>
@@ -49,6 +41,10 @@
   import Bscroll from 'better-scroll'
   export default {
     name: 'CityList',
+    props:{
+      hot : Array,
+      cities: Object
+    },
     mounted(){
       this.scroll = new Bscroll(this.$refs.wrapper)
     }
@@ -59,12 +55,12 @@
   @import '~styles/varibles.styl'
   .border-topbottom
     &:before
-      before-color:#ccc
+      border-color:#ccc
     &:after
       border-color:#ccc
   .border-bottom
     &:before
-      before-color:#ccc
+      border-color:#ccc
   .list
     overflow: hidden
     position: absolute;
